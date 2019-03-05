c3866502cbe37ddfc16eddb14f03f43af6eeebf9
TheRealVira <therealvira@gmail.com>
2019-03-05 14:24:38 +0100

    Adopted materialized theme.

--- ViraOfficialWebpage/ClientApp/src/app/app.module.ts
+++ ViraOfficialWebpage/ClientApp/src/app/app.module.ts
@@ -8,19 +8,34 @@ import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule, MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatToolbarModule, MatSidenavModule, MatListModule} from '@angular/material';
+import { LayoutModule } from '@angular/cdk/layout';
+
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    HomeComponent,
+    HomeComponent
   ],
   imports: [
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' }
-    ])
+    ]),
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
